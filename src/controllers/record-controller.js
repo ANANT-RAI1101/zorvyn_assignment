8f42cfb7b7400940ffce2d9d9c70793f5e1f24ac
ANANT-RAI1101 <anantrai1101@gmail.com>
2026-04-01 23:26:38 +0530

    added record controller

---
+++ src/controllers/record-controller.js
@@ -0,0 +1,231 @@
+const { StatusCodes } = require('http-status-codes');
+const RecordService = require("../services/record-service");
+
+const recordService = new RecordService();
+
+/*
+    1. create a record
+    2.get records
+    3.get records by filters
+    4.update a record
+    5.delete a record
+    6.get total income
+    7.get total expense
+    8.get net balance
+    9.get totalbalance by category
+    10.recent transactions
+    11.monthly summary
+*/
+class RecordController {
+
+    async createRecord(req, res) {
+        try {
+            const record = await recordService.createRecord(req.body);
+            return res.status(StatusCodes.CREATED).json({
+                success: true,
+                message: "Record created successfully",
+                data: record,
+                err: {}
+            })
+        } catch (error) {
+            return res.status(error.statusCodes).json({
+                success: false,
+                message: error.message,
+                data: {},
+                err: error.explanation
+            })
+        }
+    }
+
+    async getRecords(req, res) {
+        try {
+            const record = await recordService.getRecords(req.params.id);
+            return res.status(StatusCodes.OK).json({
+                success: true,
+                message: "Record fetched successfully",
+                data: record,
+                err: {}
+            })
+        } catch (error) {
+            return res.status(error.statusCodes).json({
+                success: false,
+                message: error.message,
+                data: {},
+                err: error.explanation
+            })
+        }
+    }
+
+    async getByFilters(req, res) {
+        try {
+            const records = await recordService.getByFilters(req.query);
+            return res.status(StatusCodes.OK).json({
+                success: true,
+                message: "Records fetched successfully",
+                data: records,
+                err: {}
+            })
+        } catch (error) {
+            return res.status(error.statusCodes).json({
+                success: false,
+                message: error.message,
+                data: {},
+                err: error.explanation
+            })
+        }
+    }
+
+    async updateRecord(req, res) {
+        try {
+            const record = await recordService.updateRecord(req.params.id, req.body);
+            return res.status(StatusCodes.OK).json({
+                success: true,
+                message: "Record updated successfully",
+                data: record,
+                err: {}
+            })
+        } catch (error) {
+            return res.status(error.statusCodes).json({
+                success: false,
+                message: error.message,
+                data: {},
+                err: error.explanation
+            })
+        }
+    }
+
+    async deleteRecord(req, res) {
+        try {
+            const record = await recordService.deleteRecord(req.params.id);
+            return res.status(StatusCodes.OK).json({
+                success: true,
+                message: "Record deleted successfully",
+                data: record,
+                err: {}
+            })
+        } catch (error) {
+            return res.status(error.statusCodes).json({
+                success: false,
+                message: error.message,
+                data: {},
+                err: error.explanation
+            })
+        }
+    }
+
+    async getTotalIncome(req, res) {
+        try {
+            const totalIncome = await recordService.getTotalIncomeAndExpense();
+            return res.status(StatusCodes.OK).json({
+                success: true,
+                message: "Total income and expense fetched successfully",
+                data: totalIncome.totalIncome,
+                err: {}
+            })
+        } catch (error) {
+            return res.status(error.statusCodes).json({
+                success: false,
+                message: error.message,
+                data: {},
+                err: error.explanation
+            })
+        }
+    }
+
+    async getTotalExpense(req, res) {
+        try {
+            const totalExpense = await recordService.getTotalIncomeAndExpense();
+            return res.status(StatusCodes.OK).json({
+                success: true,
+                message: "Total expense fetched successfully",
+                data: totalExpense.totalExpense,
+                err: {}
+            })
+        } catch (error) {
+            return res.status(error.statusCodes).json({
+                success: false,
+                message: error.message,
+                data: {},
+                err: error.explanation
+            })
+        }
+    }
+
+    async getNetBalance(req, res) {
+        try {
+            const netBalance = await recordService.getTotalIncomeAndExpense();
+            return res.status(StatusCodes.OK).json({
+                success: true,
+                message: "Net balance fetched successfully",
+                data: netBalance.netBalance,
+                err: {}
+            })
+        } catch (error) {
+            return res.status(error.statusCodes).json({
+                success: false,
+                message: error.message,
+                data: {},
+                err: error.explanation
+            })
+        }
+    }
+
+    async getExpenseByCategory(req, res) {
+        try {
+            const balance = await recordService.getTotalExpenseByCategory(req.params.category);
+            return res.status(StatusCodes.OK).json({
+                success: true,
+                message: "Total expense by category fetched successfully",
+                data: balance,
+                err: {}
+            })
+        } catch (error) {
+            return res.status(error.statusCodes).json({
+                success: false,
+                message: error.message,
+                data: {},
+                err: error.explanation
+            })
+        }
+    }
+
+    async recentActivity(req, res) {
+        try {
+            const records = await recordService.recentActivity();
+            return res.status(StatusCodes.OK).json({
+                success: true,
+                message: "Recent activity fetched successfully",
+                data: records,
+                err: {}
+            })
+        } catch (error) {
+            return res.status(error.statusCodes).json({
+                success: false,
+                message: error.message,
+                data: {},
+                err: error.explanation
+            })
+        }
+    }
+
+    async monthlySummary(req, res) {
+        try {
+            const summary = await recordService.monthlySummary(req.params.year);
+            return res.status(StatusCodes.OK).json({
+                success: true,
+                message: "Monthly summary fetched successfully",
+                data: summary,
+                err: {}
+            })
+        } catch (error) {
+            return res.status(error.statusCodes).json({
+                success: false,
+                message: error.message,
+                data: {},
+                err: error.explanation
+            })
+        }
+    }
+}
+
+module.exports = RecordController;
